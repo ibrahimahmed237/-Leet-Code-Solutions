b10e31b987a1295cf6d027ffacd79c3bb961eee6
Ibrahim Ahmed <74437946+ibrahimahmed237@users.noreply.github.com>
2024-03-24 23:45:06 +0200

    Create 53. Maximum Subarray_medium.cpp

---
+++ C++/53. Maximum Subarray_medium.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int> &nums) {
+        int sum = 0;
+        int mx = INT_MIN;
+        for (auto i: nums) {
+            sum += i;
+            mx = max(sum, mx);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return mx;
+    }
+};
